3b29c5251cc7de794693e5cc9234a60ff39566ce
sinkingpoint <colin@quirl.co.nz>
2016-02-09 08:59:39 +1300

    Upgraded version installed.
    As per iozag#8, a new version of
    git-lfs has been released. This installer uses innosetup so requires a new
    flag to be a silent install. Also included in this pull request is the updated
    installer.

--- git-lfs-install/tools/ChocolateyInstall.ps1
+++ git-lfs-install/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $packageName = 'git-lfs'
 $installerType = 'exe'
-$silentArgs = "/S"
-$32BitUrl  = 'https://github.com/github/git-lfs/releases/download/v1.1.0/git-lfs-windows-amd64-1.1.0.exe'
+$silentArgs = "/VERYSILENT"
+$32BitUrl  = 'https://github.com/github/git-lfs/releases/download/v1.1.1/git-lfs-windows-1.1.1.exe'
 $validExitCodes = @(
     0 # success
 )
