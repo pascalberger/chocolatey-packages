798b7b82948112c342d5d8263d2f295b2290b497
ThomasGassmann <thomas.gassmann@hotmail.com>
2016-02-11 11:29:13 +0100

    Added newer type script url

--- typescript/tools/chocolateyInstall.ps1
+++ typescript/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = "typescript.vs2013"
 $installerType = "exe"
 $silentArgs= "/quiet"
-$url = "https://visualstudiogallery.msdn.microsoft.com/2d42d8dc-e085-45eb-a30b-3f7d50d55304/file/151666/1/TypeScript_1.4_VS2013.exe"
+$url = "http://download.microsoft.com/download/4/4/3/443F86B7-A89F-48E6-AC96-0AAC2A910A29/TS1.7.5-VSUOOB.41209.00/TypeScript_Dev12.exe"
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes @(0)
