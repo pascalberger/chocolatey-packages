33d5511bcaa5a44c4cd8ae7c2e5c2acbca8e50ff
Pascal Berger <pascal.berger@ioz.ch>
2016-02-23 22:44:49 +0100

    Import package from https://github.com/jivkok/Chocolatey-AutomaticPackages/tree/master/SourceTree

---
+++ sourcetree/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'SourceTree'
+$installerType = 'exe'
+$url  = 'http://downloads.atlassian.com/software/sourcetree/windows/SourceTreeSetup_1.7.0.32509.exe'
+$silentArgs = '/passive'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url" -validExitCodes $validExitCodes
