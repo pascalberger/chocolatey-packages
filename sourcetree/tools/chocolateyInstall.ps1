10bf83b42f97262edb084dc9c0e83e23ec9ac909
Pascal Berger <pascal.berger@ioz.ch>
2016-02-29 08:14:34 +0100

    Update SourceTree to 1.8.2.3

--- sourcetree/tools/chocolateyInstall.ps1
+++ sourcetree/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'SourceTree'
 $installerType = 'exe'
-$url  = 'https://downloads.atlassian.com/software/sourcetree/windows/SourceTreeSetup_1.8.2.2.exe'
+$url  = 'https://downloads.atlassian.com/software/sourcetree/windows/SourceTreeSetup_1.8.2.3.exe'
 $silentArgs = '/passive'
 $validExitCodes = @(0)
 
