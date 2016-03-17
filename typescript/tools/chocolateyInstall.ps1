8367986b7a96f8e322418fef78070abb38dfa74b
Thomas Gassmann <thomas.gassmann@ioz.ch>
2016-03-17 08:06:41 +0100

    Added typescript 1.8.5.

--- typescript/tools/chocolateyInstall.ps1
+++ typescript/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = "typescript.vs2013"
 $installerType = "exe"
 $silentArgs= "/quiet"
-$url = "https://download.microsoft.com/download/4/4/3/443F86B7-A89F-48E6-AC96-0AAC2A910A29/TS1.8.2-VSUOOB.50218.00/TypeScript_Dev12.exe"
+$url = "http://download.microsoft.com/download/4/4/3/443F86B7-A89F-48E6-AC96-0AAC2A910A29/TS1.8.5-VSUOOB.50301.00/TypeScript_Dev12.exe"
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes @(0)
