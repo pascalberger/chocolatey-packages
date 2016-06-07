7a40bc8e611493c954910928f509f378f30bf3b2
Thomas Gassmann <thomas.gassmann@ioz.ch>
2016-06-07 16:02:56 +0200

    Updated package source url and changed version to enu.

--- officedevtools/tools/ChocolateyInstall.ps1
+++ officedevtools/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $packageName = 'officedevtools'
 $installerType = 'exe'
 $silentArgs = "/S /NoRestart"
-$32BitUrl  = 'https://download.microsoft.com/download/8/C/2/8C27B997-8843-462D-91CB-6F461D26BB8B/14.0.23930/deu/cba_bundle.exe'
+$32BitUrl  = 'https://download.microsoft.com/download/8/C/2/8C27B997-8843-462D-91CB-6F461D26BB8B/14.0.23930/enu/cba_bundle.exe'
 $validExitCodes = @(
     0 # success
 )
