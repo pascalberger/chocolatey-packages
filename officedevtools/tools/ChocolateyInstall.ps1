66e8922ae2349614274c84a3e887a7126f6f0bee
Thomas Gassmann <thomas.gassmann@ioz.ch>
2016-06-07 14:46:26 +0200

    Updated download url.

--- officedevtools/tools/ChocolateyInstall.ps1
+++ officedevtools/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $packageName = 'officedevtools'
 $installerType = 'exe'
 $silentArgs = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART"
-$32BitUrl  = 'https://www.microsoft.com/en-us/download/confirmation.aspx?id=51683'
+$32BitUrl  = 'https://download.microsoft.com/download/8/C/2/8C27B997-8843-462D-91CB-6F461D26BB8B/14.0.23930/deu/cba_bundle.exe'
 $validExitCodes = @(
     0 # success
 )
