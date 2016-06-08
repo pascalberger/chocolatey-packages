07bc73ed4bf7dd625d7e080fcafd1bf8e409bdbe
Thomas Gassmann <thomas.gassmann@ioz.ch>
2016-06-08 14:32:54 +0200

    Updated git-lfs to 1.2.1.

--- git-lfs-install/tools/ChocolateyInstall.ps1
+++ git-lfs-install/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $packageName = 'git-lfs'
 $installerType = 'exe'
 $silentArgs = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART"
-$32BitUrl  = 'https://github.com/github/git-lfs/releases/download/v1.2.0/git-lfs-windows-1.2.0.exe'
+$32BitUrl  = 'https://github.com/github/git-lfs/releases/download/v1.2.1/git-lfs-windows-1.2.1.exe'
 $validExitCodes = @(
     0 # success
 )
