7662f2f85346abe611b7ef9134cf6cff6b071d5b
Pascal Berger <pascal.berger@ioz.ch>
2016-08-08 17:58:00 +0200

    Remove packages no longer maintained in this repo.
    Source from https://github.com/jivkok/Chocolatey-AutomaticPackages is now again used for building these packages.

--- sourcetree/tools/chocolateyInstall.ps1
+++
@@ -1,7 +0,0 @@
-﻿$packageName = 'SourceTree'
-$installerType = 'exe'
-$url  = 'https://downloads.atlassian.com/software/sourcetree/windows/SourceTreeSetup_1.9.5.0.exe'
-$silentArgs = '/passive'
-$validExitCodes = @(0)
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url" -validExitCodes $validExitCodes
