b05ebddce23b11c8af8f2752472c51ab788eb80c
Pascal Berger <pascal.berger@gmail.com>
2016-11-18 19:55:45 +0000

    AU: 2 updated - git-lfs git-lfs.install [skip ci] https://gist.github.com/95bd06a2adf814823623f01cf4a7f871/bfd771840e4f26a4c2463fbfb0c10eda4c7e68a1

--- automatic/git-lfs.install/tools/ChocolateyInstall.ps1
+++ automatic/git-lfs.install/tools/ChocolateyInstall.ps1
@@ -1,8 +1,8 @@
 $packageName = 'git-lfs'
 $installerType = 'exe'
 $silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART'
-$32BitUrl = 'https://github.com/git-lfs/git-lfs/releases/download/v1.5.0/git-lfs-windows-1.5.0.exe'
-$checksum = '1b1b93a834bee02074ee7afd00c03d09dfaaafeeaa69368c36b520b389b1cded'
+$32BitUrl = 'https://github.com/git-lfs/git-lfs/releases/download/v1.5.1/git-lfs-windows-1.5.1.exe'
+$checksum = '71bfef853f651aaba3788124a9430324c679923f6c48d3fb72c11cd327b4d1c9'
 $checksumType = 'sha256'
 $validExitCodes = @(
     0 # success
