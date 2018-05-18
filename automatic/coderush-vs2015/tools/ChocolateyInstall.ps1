6250262438329d362dfe6ee1a4dcdec0bfe00973
Pascal Berger <pascal.berger@gmail.com>
2018-05-18 12:05:08 +0000

    AU: 1 updated - coderush-vs2015 [skip ci] https://gist.github.com/95bd06a2adf814823623f01cf4a7f871/ec64f26903fdf344732c4a510b7427615a619474

--- automatic/coderush-vs2015/tools/ChocolateyInstall.ps1
+++ automatic/coderush-vs2015/tools/ChocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$parameters = @{
     PackageName = "coderush-vs2015"
-    VsixUrl = "https://devexpress.gallerycdn.vsassets.io/extensions/devexpress/coderushforroslyn/17.2.8/1523881891808/DevExpress.CodeRush.Roslyn-17.2.8.vsix"
+    VsixUrl = "https://devexpress.gallerycdn.vsassets.io/extensions/devexpress/coderushforroslyn/18.1.3/1526629730682/DevExpress.CodeRush.Roslyn-18.1.3.vsix"
     VsVersion = 14 
-    Checksum = "f4cdd7e886e767faa37f7c9965a17245af372d1284386b46d5e6aedc71ee46a8"
+    Checksum = "36e6e5122cded168d72848cf1262c1c625117026df158dca46149702504033fa"
     ChecksumType = "sha256"
 }
 
