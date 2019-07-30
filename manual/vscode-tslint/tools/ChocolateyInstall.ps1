97f010a51e3ffb7b1871a1684612ec3653ab58c4
Pascal Berger <pascal.berger@gmail.com>
2019-07-30 20:29:03 +0200

    (GH-129) Update to new ms-vscode.vscode-typescript-tslint-plugin extension

--- manual/vscode-tslint/tools/ChocolateyInstall.ps1
+++ manual/vscode-tslint/tools/ChocolateyInstall.ps1
@@ -1,2 +1,10 @@
 Update-SessionEnvironment
-code --install-extension eg2.tslint
+
+$deprecatedVersionExists = code --list-extensions | Where-Object { $_ -eq "eg2.tslint" }
+if ($deprecatedVersionExists)
+{
+    Write-Warning "The deprecated vscode-tslint extension is already installed. Uninstalling it to avoid linting files twice."
+    code --uninstall-extension eg2.tslint
+}
+
+code --install-extension ms-vscode.vscode-typescript-tslint-plugin
