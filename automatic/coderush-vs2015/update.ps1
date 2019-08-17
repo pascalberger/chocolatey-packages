d18fb67c4bab52f8eabb87276fd87a34bf36faf4
Pascal Berger <pascal.berger@gmail.com>
2019-08-17 22:24:10 +0200

    (coderush-vs2015) Disable update

--- automatic/coderush-vs2015/update.ps1
+++ automatic/coderush-vs2015/update.ps1
@@ -2,6 +2,8 @@ import-module au
 
 $releases = 'https://marketplace.visualstudio.com/items?itemName=DevExpress.CodeRushforRoslyn'
 
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix -FileNameBase $Latest.FileName }
+
 function global:au_SearchReplace {
     @{
         'tools\chocolateyInstall.ps1' = @{
@@ -13,19 +15,20 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
-    $json = $download_page.AllElements | ? class -eq 'vss-extension' | select -expand innerHtml | ConvertFrom-Json | select -expand versions
-    $url = $json.files | ? source -match "\.vsix$" | select -expand source -first 1
-
-    $version = $json.version | select -first 1
-    $checksum = Get-RemoteChecksum $url
+    $re      = 'CodeRushforRoslyn/.+?/vspackage$'
+    $url     = $download_page.links | Where-Object href -match $re | ForEach-Object { "https://marketplace.visualstudio.com" + $_.href  }
+    $version = $url -split '/' | Select-Object -Last 1 -Skip 1
 
     @{
         Version   = $version
         URL32     = $url
-        Checksum  = $checksum
+        Filename  = "DevExpress.CodeRush-${version}"
     }
 }
+
+Write-Host "ignored until fixed as updater blocks"
+return 'ignore'
 
 update -ChecksumFor none
