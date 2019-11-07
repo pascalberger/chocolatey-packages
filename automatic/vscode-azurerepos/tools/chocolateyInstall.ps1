f06b291aad06bee268709cb3dbf855dc5183d4d3
Pascal Berger <pascal.berger@gmail.com>
2019-11-07 18:01:05 +0000

    AU: 1 updated - vscode-azurerepos [skip ci] https://gist.github.com/95bd06a2adf814823623f01cf4a7f871/59335b5fd2aa5d63d89f58c70ecd9c9d82d9615b

--- automatic/vscode-azurerepos/tools/chocolateyInstall.ps1
+++ automatic/vscode-azurerepos/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $extensionName = "team-"
-$extensionVersion = "1.149.2"
+$extensionVersion = "1.161.0"
 $extensionId = "$toolsDir\$extensionName$extensionVersion.vsix"
 
 Update-SessionEnvironment
