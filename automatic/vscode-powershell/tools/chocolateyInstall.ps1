e4ae8d310b7228e0743b569b9236b76543fd126e
Pascal Berger <pascal.berger@gmail.com>
2022-05-04 12:01:24 +0000

    AU: 1 updated - vscode-powershell [skip ci] https://gist.github.com/95bd06a2adf814823623f01cf4a7f871/b3f00979df23053c8f2e095f8028a48f970838b3

--- automatic/vscode-powershell/tools/chocolateyInstall.ps1
+++ automatic/vscode-powershell/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $extensionName = "PowerShell-"
-$extensionVersion = "2021.12.0"
+$extensionVersion = "2022.5.0"
 $extensionId = "$toolsDir\$extensionName$extensionVersion.vsix"
 
 Update-SessionEnvironment
