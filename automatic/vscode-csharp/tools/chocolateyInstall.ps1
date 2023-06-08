890cf9014f2bc1f6be29758c5fca19b0471c6437
Pascal Berger <pascal.berger@gmail.com>
2023-06-08 00:29:40 +0000

    AU: 1 updated - vscode-csharp [skip ci] https://gist.github.com/pascalberger/95bd06a2adf814823623f01cf4a7f871/fac8af93483db06f712ae0f1e8c840472c0fd9e9

--- automatic/vscode-csharp/tools/chocolateyInstall.ps1
+++ automatic/vscode-csharp/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $extensionName = "csharp-"
-$extensionVersion = "1.25.8"
+$extensionVersion = "1.25.9"
 $extensionId = "$toolsDir\$extensionName$extensionVersion-win32-x64.vsix"
 
 Update-SessionEnvironment
