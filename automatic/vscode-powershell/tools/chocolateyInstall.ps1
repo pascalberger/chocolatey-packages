6c14f604c78270ef752c8b733a4e05e28b1dd3c8
Pascal Berger <pascal.berger@gmail.com>
2024-04-09 08:12:31 +0000

    AU: 2 updated - azuredatastudio-powershell vscode-powershell [skip ci] https://gist.github.com/pascalberger/95bd06a2adf814823623f01cf4a7f871/27c58ef42b0ea60a0a6d684a35da08153b5658f8

--- automatic/vscode-powershell/tools/chocolateyInstall.ps1
+++ automatic/vscode-powershell/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $extensionName = "PowerShell-"
-$extensionVersion = "2024.0.0"
+$extensionVersion = "2024.2.0"
 $extensionId = "$toolsDir\$extensionName$extensionVersion.vsix"
 
 Update-SessionEnvironment
