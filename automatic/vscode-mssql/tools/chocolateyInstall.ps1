0eded99bbe449e8281afabf09c4da7a2ff36205f
Pascal Berger <pascal.berger@gmail.com>
2025-08-14 00:39:25 +0000

    AU: 1 updated - vscode-mssql [skip ci] https://gist.github.com/pascalberger/95bd06a2adf814823623f01cf4a7f871/f3cf1fdca9176a0375fcebb7ee645dcbc45197f6

--- automatic/vscode-mssql/tools/chocolateyInstall.ps1
+++ automatic/vscode-mssql/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $extensionName = "mssql-"
-$extensionVersion = "1.34.0"
+$extensionVersion = "1.34.1"
 $osBitness = Get-OSArchitectureWidth
 $extensionId = "$toolsDir\$extensionName$extensionVersion-win7-x$osBitness.vsix"
 
